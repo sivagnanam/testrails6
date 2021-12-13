e7563f2aa41b64a984053151853d5f150d8f3b49
Siva <sivamailme@gmail.com>
2021-12-13 11:51:38 +0530

    articles feature completed

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,9 @@
 class ArticlesController < ApplicationController
+    before_action :set_article, only: [:show, :edit, :update,:destroy] 
+
     def show
         #byebug
-        @article = Article.find(params[:id])
+        #@article = Article.find(params[:id])
     end
 
     def index
@@ -13,11 +15,12 @@ class ArticlesController < ApplicationController
     end
 
     def edit
-        @article = Article.find(params[:id])
+       # @article = Article.find(params[:id])
     end
+
     def create
         #render plain: params[:article]
-        @article = Article.new(params.require(:article).permit(:title,:description))
+        @article = Article.new(article_params)
         #render plain: @article.inspect
         if @article.save
             flash[:notice] ="Successfully Saved"
@@ -28,14 +31,28 @@ class ArticlesController < ApplicationController
     end 
 
     def update 
-        @article = Article.find(params[:id])
-        if @article.update(params.require(:article).permit(:title,:description))
+        #@article = Article.find(params[:id])
+        if @article.update(article_params)
             flash[:notice] ="Successfully Updated"
             redirect_to  article_path(@article)
         else
             render 'edit' 
         end
-
     end
 
+    def destroy
+        #@article = Article.find(params[:id])
+        @article.destroy
+        redirect_to articles_path
+    end
+
+    private
+
+    def set_article
+        @article = Article.find(params[:id])
+    end
+
+    def article_params
+        params.require(:article).permit(:title,:description)
+    end
 end
