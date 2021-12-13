9e94cb54bf5026a2a7971b5f8a410c96e58fafc4
Siva <sivamailme@gmail.com>
2021-12-13 10:10:18 +0530

    article editpage

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,4 +8,34 @@ class ArticlesController < ApplicationController
         @articles =Article.all
     end
 
+    def new
+        @article = Article.new
+    end
+
+    def edit
+        @article = Article.find(params[:id])
+    end
+    def create
+        #render plain: params[:article]
+        @article = Article.new(params.require(:article).permit(:title,:description))
+        #render plain: @article.inspect
+        if @article.save
+            flash[:notice] ="Successfully Saved"
+            redirect_to  article_path(@article)
+        else
+            render 'new' 
+        end
+    end 
+
+    def update 
+        @article = Article.find(params[:id])
+        if @article.update(params.require(:article).permit(:title,:description))
+            flash[:notice] ="Successfully Updated"
+            redirect_to  article_path(@article)
+        else
+            render 'edit' 
+        end
+
+    end
+
 end
