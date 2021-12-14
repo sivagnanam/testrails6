e056879bee8ef4f17ae8de535d11b65c29fcdc40
Siva <sivamailme@gmail.com>
2021-12-14 13:07:53 +0530

    edit user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,19 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update (user_params)
+        flash[:notice] =" You've successfully updated your profile"
+        redirect_to articles_path
+    else
+        render 'edit'
+    end
+  end
   def create
     #byebug
     @user = User.new(user_params)
@@ -15,9 +28,12 @@ class UsersController < ApplicationController
     end
   end
 
+  
+
   private
 
   def user_params 
     params.require(:user).permit(:username,:email,:password)
   end
+
 end
