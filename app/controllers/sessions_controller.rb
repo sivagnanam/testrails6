2db1705bd1bf3eb65bfb38891f3b53ed61235da1
Siva <sivamailme@gmail.com>
2021-12-14 15:53:22 +0530

    login and logout basic methods

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,21 @@ class SessionsController < ApplicationController
     end
 
     def create 
-        byebug
+        #byebug
+        user = User.find_by(email: params[:session][:email].downcase)
+        if user && user.authenticate(params[:session][:password])
+            session[:user_id] =user.id 
+            flash[:notice] = "Logged in succesfully"
+            redirect_to user
+        else
+            flash.now[:alert] ="Wrong Credentials" 
+            render 'new'
+        end
     end
 
     def destroy
-
+        session[:user_id] = nil 
+        flash[:notice] = "Logged Out"
+        redirect_to root_path
     end
 end
