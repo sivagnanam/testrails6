bacc05f45cc180ac4938ee19765907934083aa4f
Siva <sivamailme@gmail.com>
2021-12-14 12:55:30 +0530

    user signup

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
   resources :articles
   get 'signup', to: 'users#new'
   #post 'users', to: 'users#create'
-  resources :articles ,except: [:new] #new not needed since we have signup
+  resources :users ,except: [:new] #new not needed since we have signup
 end
