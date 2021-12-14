db3f85e5afdef1fdb1b96fe91c93f8e0f1e00c2e
Siva <sivamailme@gmail.com>
2021-12-14 18:22:19 +0530

    admin feature

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update]
-  before_action :require_user, only: [:edit, :update]
-  before_action :require_same_user, only: [:edit, :update]
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :require_user, only: [:edit, :update, :destroy]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
   def new
     @user = User.new
   end
@@ -42,6 +42,13 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    @user.destroy
+    session[:user_id] = nil if @user == current_user
+    flash[:notice] = "Account and all associated articles are deleted"
+    redirect_to articles_path
+  end
+
   private
 
   def user_params 
@@ -53,8 +60,8 @@ class UsersController < ApplicationController
   end
 
   def require_same_user 
-    if current_user!=@user
-        flash[:alert] = "you can only edit your profile"
+    if current_user!=@user && !current_user.admin?
+        flash[:alert] = "you can only edit or delete your profile"
         redirect_to @user
     end
   end
