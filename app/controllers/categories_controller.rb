fcdf16a01967ed733c98b4b76a2c77de7349d024
Siva <sivamailme@gmail.com>
2021-12-15 12:15:45 +0530

    category feature completed

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -23,8 +23,18 @@ class CategoriesController < ApplicationController
     end
 
     def edit
+        @category = Category.find(params[:id])
     end
 
+    def update 
+        @category = Category.find(params[:id])
+        if @category.update(category_params)
+            flash[:notice]="Category Updated"
+            redirect_to @category
+        else
+            render 'edit'
+        end
+    end
     private 
 
     def category_params
